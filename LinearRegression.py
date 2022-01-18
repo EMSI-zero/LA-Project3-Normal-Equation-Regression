f88f12ce566d96ef64bd6c95b030dda33029a490
emad ferdowsi <emad.ferdowsi@outlook.com>
2022-01-18 22:02:16 +0330

    Add Prediction Functions
    -Adds linear and quadratic prediction function
    -Edits documentation

--- LinearRegression.py
+++ LinearRegression.py
@@ -10,7 +10,7 @@ def linearRegression(dataFrame):
         dataFrame (list): raw data input 
 
     Returns:
-        tuple : a0 & a1 in equation {Y = a0 + a1.X}
+        (a0 , a1)(tuple) : a0 & a1 in equation {Y = a0 + a1.X}
     """
     return (a0 , a1)
 
@@ -21,7 +21,35 @@ def quadraticRegression(dataFrame):
         dataFrame (list): raw data input 
 
     Returns:
-        tuple : a0 ,a1 , a2 in equation {Y = a0 + a1.X + a2.X^2}
+        (a0 , a1 , a2) (tuple) : a0 ,a1 , a2 in equation {Y = a0 + a1.X + a2.X^2}
     """
     return (a0 , a1 , a2)
+
+
+def linearPredictaion(x , a0 , a1):
+    """Predicts the Expected output by using the coefficients calculated by linearRegression.
+
+    Args:
+        x (double): x input
+        a0 (double): a0 of the equation {Y= a0 + a1.X}
+        a1 (double): a1 of the equation {Y= a0 + a1.X}
+    
+    Returns:
+        Y(double): answer calculated by linearRegression prediction
+    """
+    pass
+
+def quadraticPrediction(x , a0 , a1 , a2):
+    """Predicts the Expected output by using the coefficients calculated by quadraticRegression.
+
+    Args:
+        x (double): x input
+        a0 (double): a0 of the equation {Y= a0 + a1.X+a2X^2}
+        a1 (double): a1 of the equation {Y= a0 + a1.X+a2X^2}
+        a2 (double): a2 of the equation {Y= a0 + a1.X+a2X^2}
+    
+    Returns:
+        Y(double): answer calculated by quadraticRegression prediction
+    """
+    pass
     
