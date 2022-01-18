8d061e2642bf856780813fbb47cfcb121d402805
emad ferdowsi <emad.ferdowsi@outlook.com>
2022-01-18 21:47:44 +0330

    Add Docs and Reform Regression Functions
    -Adds documentation and sets the output of regression functions to tuples of coefficients

--- LinearRegression.py
+++ LinearRegression.py
@@ -1,9 +1,27 @@
+from tkinter import Y
 import numpy
 import pandas
 import matplotlib
 
 def linearRegression(dataFrame):
-    pass
+    """Finds a Linear Regression for a set of data from dataFrame input.
+
+    Args:
+        dataFrame (list): raw data input 
+
+    Returns:
+        tuple : a0 & a1 in equation {Y = a0 + a1.X}
+    """
+    return (a0 , a1)
 
 def quadraticRegression(dataFrame):
-    pass
+    """Finds a Quadratic Regression for a set of data from dataFrame input.
+
+    Args:
+        dataFrame (list): raw data input 
+
+    Returns:
+        tuple : a0 ,a1 , a2 in equation {Y = a0 + a1.X + a2.X^2}
+    """
+    return (a0 , a1 , a2)
+    
